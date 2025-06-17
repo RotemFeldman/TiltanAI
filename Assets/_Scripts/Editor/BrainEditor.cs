ce24230376a4a98d4d613ff983333941015774b1
AdiGaash <adigaash@gmail.com>
2025-06-17 15:03:57 +0300

    fix

--- Assets/_Scripts/Editor/BrainEditor.cs
+++ Assets/_Scripts/Editor/BrainEditor.cs
@@ -38,20 +38,9 @@ namespace UtilityAI {
 
         private void DrawConsideration(Consideration consideration, Context context, int indentLevel) {
             EditorGUI.indentLevel = indentLevel;
-
-            if (consideration is CompositeConsideration compositeConsideration) {
-                EditorGUILayout.LabelField(
-                    $"Composite Consideration: {compositeConsideration.name}, Operation: {compositeConsideration.operation}"
-                );
-
-                foreach (Consideration subConsideration in compositeConsideration.considerations) {
-                    DrawConsideration(subConsideration, context, indentLevel + 1);
-                }
-            } else {
-                float value = consideration.Evaluate(context);
-                EditorGUILayout.LabelField($"Consideration: {consideration.name}, Value: {value:F2}");
-            }
-
+            
+            float value = consideration.Evaluate(context);
+            EditorGUILayout.LabelField($"Consideration: {consideration.name}, Value: {value:F2}");
             EditorGUI.indentLevel = indentLevel - 1; // Reset indentation after drawing
         }
 
