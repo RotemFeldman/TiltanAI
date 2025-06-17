8bd25d77b59165d58edbf073bc8bd0e4c0a29a32
AdiGaash <adigaash@gmail.com>
2025-06-17 15:01:55 +0300

    Basics of Utility AI (with scriptableobjects)

---
+++ Assets/_Scripts/UtilityAI/Brain.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace UtilityAI {
+    [RequireComponent(typeof(NavMeshAgent), typeof(Sensor))]
+    public class Brain : MonoBehaviour {
+        public List<AIAction> actions;
+        public Context context;
+
+        
+
+        void Awake() {
+            context = new Context(this);
+            
+
+            foreach (var action in actions) {
+                action.Initialize(context);
+            }
+        }
+
+        void Update() {
+            UpdateContext();
+            
+            AIAction bestAction = null;
+            float highestUtility = float.MinValue;
+
+            foreach (var action in actions) {
+                float utility = action.CalculateUtility(context);
+                if (utility > highestUtility) {
+                    highestUtility = utility;
+                    bestAction = action;
+                }
+            }
+
+            if (bestAction != null) {
+                bestAction.Execute(context);
+            }
+        }
+
+        void UpdateContext() {
+            // update context with current state
+        }
+    }
+}
---
+++ Assets/_Scripts/UtilityAI/Context.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+namespace UtilityAI {
+    public class Context {
+        public Brain brain;
+        public NavMeshAgent agent;
+        public Transform target;
+        public Sensor sensor;
+        
+        readonly Dictionary<string, object> data = new();
+
+        public Context(Brain brain) {
+           
+            
+            this.brain = brain;
+            this.agent = brain.gameObject.GetComponent<NavMeshAgent>();
+            this.sensor = brain.gameObject.GetComponent<Sensor>();
+        }
+        
+        public T GetData<T>(string key) => data.TryGetValue(key, out var value) ? (T)value : default;
+        public void SetData(string key, object value) => data[key] = value;
+    }
+}
