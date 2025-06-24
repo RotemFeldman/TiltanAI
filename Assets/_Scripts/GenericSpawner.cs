92d778bb07695f6fd5e79281efa1da053674e2bb
AdiGaash <adigaash@gmail.com>
2025-06-24 16:24:19 +0300

    spawnablegenericfornextstep

---
+++ Assets/_Scripts/GenericSpawner.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class GenericSpawner : MonoBehaviour
+{
+    [Header("Spawn Configuration")]
+    public SpawnableItemSO[] spawnableItems;
+    public float spawnInterval = 5f;
+    public Transform[] spawnPoints;
+    
+    [Header("Spawn Settings")]
+    public bool useWeightedSpawning = true;
+    public bool spawnOnStart = true;
+    
+    private int totalWeight;
+    
+    private void Start()
+    {
+        if (spawnableItems == null || spawnableItems.Length == 0)
+        {
+            Debug.LogError("No spawnable items assigned!");
+            return;
+        }
+
+        CalculateTotalWeight();
+        
+        if (spawnOnStart)
+        {
+            StartCoroutine(SpawnRoutine());
+        }
+    }
+    
+    private void CalculateTotalWeight()
+    {
+        totalWeight = spawnableItems.Sum(item => item.spawnWeight);
+    }
+    
+    private IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnInterval);
+            
+            if (useWeightedSpawning)
+            {
+                SpawnWeighted();
+            }
+            else
+            {
+                SpawnRandom();
+            }
+        }
+    }
+    
+    private void SpawnWeighted()
+    {
+        int randomWeight = Random.Range(0, totalWeight);
+        int currentWeight = 0;
+        
+        foreach (var item in spawnableItems)
+        {
+            currentWeight += item.spawnWeight;
+            if (randomWeight < currentWeight && CountExistingItems(item) < item.maxActiveItems)
+            {
+                SpawnItem(item);
+                break;
+            }
+        }
+    }
+    
+    private void SpawnRandom()
+    {
+        var availableItems = spawnableItems
+            .Where(item => CountExistingItems(item) < item.maxActiveItems)
+            .ToArray();
+            
+        if (availableItems.Length > 0)
+        {
+            SpawnItem(availableItems[Random.Range(0, availableItems.Length)]);
+        }
+    }
+    
+    private int CountExistingItems(SpawnableItemSO item)
+    {
+        if (!string.IsNullOrEmpty(item.tag))
+        {
+            return GameObject.FindGameObjectsWithTag(item.tag).Length;
+        }
+        
+        return GameObject.FindObjectsOfType<GameObject>()
+            .Count(obj => obj.name == item.prefab.name || obj.name == item.prefab.name + "(Clone)");
+    }
+    
+    private void SpawnItem(SpawnableItemSO item)
+    {
+        if (spawnPoints == null || spawnPoints.Length == 0)
+        {
+            Debug.LogWarning("No spawn points assigned!");
+            return;
+        }
+        
+        if (item.prefab == null)
+        {
+            Debug.LogError($"Prefab is not assigned in {item.name}!");
+            return;
+        }
+        
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        
+        Quaternion rotation = item.randomizeRotation ? 
+            Quaternion.Euler(item.GetRandomRotation()) : 
+            spawnPoint.rotation;
+            
+        Instantiate(item.prefab, spawnPoint.position, rotation);
+    }
+    
+    public void StartSpawning()
+    {
+        StopAllCoroutines();
+        StartCoroutine(SpawnRoutine());
+    }
+    
+    public void StopSpawning()
+    {
+        StopAllCoroutines();
+    }
+}
---
+++ Assets/_Scripts/SpawnableItemSO.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Spawnable Item", menuName = "Spawning/Spawnable Item")]
+public class SpawnableItemSO : ScriptableObject
+{
+    [Header("Prefab Settings")]
+    public GameObject prefab;
+    public string tag;
+    
+    [Header("Spawn Settings")]
+    [Min(1)]
+    public int maxActiveItems = 3;
+    [Tooltip("Weight affects the probability of this item being chosen when spawning")]
+    [Range(1, 100)]
+    public int spawnWeight = 1;
+    
+    [Header("Optional Settings")]
+    [Tooltip("If true, the item will rotate randomly when spawned")]
+    public bool randomizeRotation;
+    [Tooltip("Random rotation will be applied only on these axes")]
+    public bool rotateX, rotateY, rotateZ;
+    
+    public Vector3 GetRandomRotation()
+    {
+        return new Vector3(
+            rotateX ? Random.Range(0f, 360f) : 0f,
+            rotateY ? Random.Range(0f, 360f) : 0f,
+            rotateZ ? Random.Range(0f, 360f) : 0f
+        );
+    }
+}
